679b59601bfb496ee43b3c629e7d7e3ded405fb8
EllenCoe <ellen.cristina.prestserv@petrobras.com.br>
2025-03-18 13:43:34 -0300

    Testing reduce coverage

---
+++ src/untestedComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UntestedComponent = () => {
+  return (
+    <div>
+      <h2>Este componente não é testado!</h2>
+      <p>Ele foi criado para reduzir a cobertura de código.</p>
+    </div>
+  );
+};
+
+export default UntestedComponent;
